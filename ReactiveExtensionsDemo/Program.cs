3ec94a2c71d775cf49be977aec8be7d288dcd4de
Nimble-Ste <ste@nimbleapproach.com>
2023-11-10 11:31:17 +0000

    quick demo for how to use rx in C#

---
+++ ReactiveExtensionsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReactiveExtensionsDemo
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using static System.Formats.Asn1.AsnWriter;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services.AddSingleton<TemperatureService>();
+
+            IHost host = builder.Build();
+
+
+            using var scope = host.Services.CreateScope();
+            var tempService = scope.ServiceProvider.GetRequiredService<TemperatureService>();
+
+            tempService.TemperatureObservable.Subscribe(x =>
+            {
+                Console.WriteLine($"The Temperature Is Now {x}C");
+            });
+
+            host.Run();
+        }
+    }
+}
---
+++ ReactiveExtensionsDemo/TemperatureService.cs
@@ -0,0 +1,22 @@
+﻿namespace ReactiveExtensionsDemo
+{
+    using System.Reactive.Linq;
+    using System.Reactive.Subjects;
+
+    public class TemperatureService
+    {
+        private Subject<int> Temperature { get; } = new();
+
+        public IObservable<int> TemperatureObservable => this.Temperature.AsObservable();
+
+        public TemperatureService()
+        {
+            Observable
+                .Interval(TimeSpan.FromSeconds(1)).StartWith(0).Subscribe(async _ =>
+                {
+                    var rnd = new Random();
+                    Temperature.OnNext(rnd.Next(-20, 30));
+                });
+        }
+    }
+}
